0999ce559cefd8cda2c90c5cccd754a8d9591b8d
AN Du Van (VTI.D2) <an.duvan@vti.vn>
2023-04-17 10:48:58 +0700

    first commit

---
+++ tranning-springboot-17-04-2023/src/main/java/com/example/tranningspringboot17042023/TranningSpringboot17042023Application.java
@@ -0,0 +1,13 @@
+package com.example.tranningspringboot17042023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranningSpringboot17042023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranningSpringboot17042023Application.class, args);
+	}
+
+}
---
+++ tranning-springboot-17-04-2023/src/test/java/com/example/tranningspringboot17042023/TranningSpringboot17042023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tranningspringboot17042023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranningSpringboot17042023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
